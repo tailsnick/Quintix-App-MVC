742a2fc72350c3308ec969c83d901cd316a457ee
Rachel Hoffman <tailsnick5@gmail.com>
2022-05-26 15:40:36 -0600

    Fixed Login

--- Quintix App MVC/Data/Migrations/20220526004037_ChangesInApplicationDbContext.Designer.cs
+++ Quintix App MVC/Data/Migrations/20220526004037_ChangesInApplicationDbContext.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Quintix_App_MVC.Data;
 
@@ -11,9 +12,10 @@ using Quintix_App_MVC.Data;
 namespace Quintix_App_MVC.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220526004037_ChangesInApplicationDbContext")]
+    partial class ChangesInApplicationDbContext
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
